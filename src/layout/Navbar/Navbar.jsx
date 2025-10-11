1bb028258190c69d2a0af6d6ac636f4d6ea06abf
papidze-nika <papidzenika24@gmail.com>
2025-10-11 20:19:30 +0400

    Initial commit

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> PedroTech </span>{" "}
+            <span className="text-glow text-foreground"> NikaCore </span>{" "}
             Portfolio
           </span>
         </a>
